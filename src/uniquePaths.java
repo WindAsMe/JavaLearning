b77fdc61bb0f830787a85b2b24a41b6425cd4db1
WindAsMe <542636539@qq.com>
2018-05-31 13:12:31 +0800

    Java For LeeCode

--- src/uniquePaths.java
+++ src/uniquePaths.java
@@ -27,7 +27,22 @@ public class uniquePaths {
 
     }
 
+    // Iteration
+    private static int uniquePathsResult2(int m, int n){
+        int[][] result = new int[m][n];
+        for (int i = 0 ; i < m ; i ++ )
+            result[i][0] = 1;
+        for (int i = 0 ; i < n ; i ++ )
+            result[0][i] = 1;
+        for(int i = 1; i < m; i++){
+            for(int j = 1; j < n; j++){
+                result [i][j] = result[i-1][j] + result[i][j-1];
+            }
+        }
+        return result[m-1][n-1];
+    }
+
     public static void main(String[] args){
-        System.out.println(uniquePathsResult(51, 9));
+        System.out.println(uniquePathsResult2(7, 3));
     }
 }
