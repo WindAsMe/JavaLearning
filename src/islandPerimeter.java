bb0c6fdeceb528e496e3b8c5b5fc756f5b79597b
WindAsMe <542636539@qq.com>
2018-06-14 17:20:37 +0800

    Java For LeeCode

--- src/islandPerimeter.java
+++ src/islandPerimeter.java
@@ -12,7 +12,10 @@ public class islandPerimeter {
         for (int i = 0; i < grid.length; i++) {
             for (int j = 0; j < grid[0].length; j++) {
                 if (grid[i][j] == 1) {
+                    // Most is 4
                     int n = 4;
+                    // up down left right has '1', need -1
+                    // the statics need be rational
                     if (j - 1 >= 0 && grid[i][j - 1] == 1) n--;
                     if (j + 1 < grid[0].length && grid[i][j + 1] == 1) n--;
                     if (i - 1 >= 0 && grid[i - 1][j] == 1) n--;
