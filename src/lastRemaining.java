03b05dfc334afbabc48c820b252ba63a3921ac25
WindAsMe <542636539@qq.com>
2018-06-14 14:03:10 +0800

    Java For LeeCode

--- src/lastRemaining.java
+++ src/lastRemaining.java
@@ -9,6 +9,55 @@ import java.util.Arrays;
  */
 public class lastRemaining {
 
+    private static int lastRemainingResult1(int n) {
+        if(n == 1)
+            return 1;
+        if(n == 2)
+            return 2;
+        // Transfer to even
+        // f(2k + 1) = f(2k)
+        if((n & 0x1) == 0x1){
+            n --;
+        }
+        if(((n - 1) & n) == 0){
+            // 2 ^ x
+            int index = 0;
+            int tmpN = n;
+            tmpN = tmpN >>> 1;
+            while(tmpN > 0){
+                // n = 2 ^ k
+                // index represent the k
+                index ++;
+                tmpN = tmpN >>> 1;
+            }
+            int ans;
+            if((index & 1) == 1){
+                // index is odd
+                // correspond to f(n) = (2n + 2) / 3,
+                // tmpN = 2n
+                tmpN=n << 1;
+            }
+            else{
+                // index is even
+                // correspond to f(n)=(n + 2) / 3
+                // tmpN = n
+                tmpN = n;
+            }
+            // tmpN = n or tmpN = 2n
+            ans = (tmpN + 2) / 3;
+            return ans;
+
+        }
+        else{
+            // Ensure n % 2 == 0
+            int tmpAns = lastRemainingResult1(n >>> 1);
+            return n - 2 * tmpAns + 2;
+        }
+    }
+
+
+
+    // This management is TL
     private static int lastRemainingResult(int n) {
         if (n == 1)
             return 1;
@@ -45,6 +94,6 @@ public class lastRemaining {
 
 
     public static void main(String[] args) {
-        System.out.println(lastRemainingResult(1000000000));
+        System.out.println(lastRemainingResult1(100000));
     }
 }
