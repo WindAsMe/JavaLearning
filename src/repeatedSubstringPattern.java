9e43e2525236ac631a3f80b21771854a77ea608a
WindAsMe <542636539@qq.com>
2018-06-15 14:41:14 +0800

    Java For LeeCode

--- src/repeatedSubstringPattern.java
+++ src/repeatedSubstringPattern.java
@@ -6,4 +6,26 @@
  * Function   :
  */
 public class repeatedSubstringPattern {
+
+    private static boolean repeatedSubstringPatternResult(String str) {
+        int len = str.length();
+        // If has the repeated string
+        // Longest is len / 2
+        for(int i = 1 ; i <= len / 2 ; i ++ ) {
+            if(len % i == 0) {
+                String tem = str.substring(0, i);
+                StringBuffer sb = new StringBuffer();
+                for(int j = 0 ; j < len / i ; j ++ ) {
+                    sb = sb.append(tem);
+                }
+                if(sb.toString().equals(str))
+                    return true;
+            }
+        }
+        return false;
+    }
+
+    public static void main(String[] args) {
+        System.out.println(repeatedSubstringPatternResult("asdasd"));
+    }
 }
