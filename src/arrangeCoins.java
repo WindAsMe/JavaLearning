897f2400e7ee34a2e86fa90f842573ed914779e8
WindAsMe <542636539@qq.com>
2018-06-16 13:32:10 +0800

    Java For LeeCode

---
+++ src/arrangeCoins.java
@@ -0,0 +1,30 @@
+/**
+ * Author     : WindAsMe
+ * File       : arrangeCoins.java
+ * Time       : Create on 18-6-16
+ * Location   : ../Home/JavaForLeeCode2/arrangeCoins.java
+ * Function   : LeeCode No.441
+ */
+public class arrangeCoins {
+
+    private static int arrangeCoinsResult(int n) {
+        int start = 0;
+        int end = n;
+        while (start <= end) {
+            int mid = start + (end - start) / 2;
+            long x = mid * (mid + 1L) / 2;
+            if (x == n)
+                return mid;
+            else if (x < n)
+                start = mid + 1;
+            else
+                end = mid - 1;
+        }
+        return end;
+    }
+
+
+    public static void main(String[] args) {
+        System.out.println(arrangeCoinsResult(7534523));
+    }
+}
