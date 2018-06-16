d16dc6abd0ae0021adb7587a1f03d05546ca41d2
WindAsMe <542636539@qq.com>
2018-06-16 14:59:30 +0800

    Java For LeeCode

--- src/minMoves2.java
+++ src/minMoves2.java
@@ -9,34 +9,56 @@ import java.util.Arrays;
  */
 public class minMoves2 {
 
+    // Find the middle
     private static int minMoves2Result(int[] nums) {
-        double average =  sum(nums) / nums.length;
-        int count = 0;
-
-        System.out.println("average" + average + "  " + (int) (average + 1));
-
-        if (average - (int) average >= 0.5) {
-            System.out.println("here");
-            for (int num : nums) {
-                count += Math.abs((int) (average + 1) - num);
+        int n = nums.length;
+        if( n <= 1 ){
+            return 0;
+        }
+        else {
+            // Find the middle number
+            Arrays.sort(nums);
+            int mean = nums[n / 2];
+            int i;
+            // Movement step
+            int res = 0;
+            for( i = 0 ; i < n ; i ++ ){
+                res += Math.abs(nums[i]-mean);
             }
-            return count;
-        } else {
-            System.out.println("here1");
-            for (int num : nums) {
-                count += Math.abs((int) average - num);
-            }
-            return count;
+            return res;
         }
     }
 
-    private static double sum(int[] nums) {
-        long sum = 0;
-        for (int a : nums) {
-            sum += a;
-        }
-        return sum;
-    }
+    // It's not true to find the average
+    // The number needed is middle
+//    private static int minMoves2Result(int[] nums) {
+//        double average =  sum(nums) / nums.length;
+//        int count = 0;
+//
+//        System.out.println("average" + average + "  " + (int) (average + 1));
+//
+//        if (average - (int) average >= 0.5) {
+//            System.out.println("here");
+//            for (int num : nums) {
+//                count += Math.abs((int) (average + 1) - num);
+//            }
+//            return count;
+//        } else {
+//            System.out.println("here1");
+//            for (int num : nums) {
+//                count += Math.abs((int) average - num);
+//            }
+//            return count;
+//        }
+//    }
+//
+//    private static double sum(int[] nums) {
+//        long sum = 0;
+//        for (int a : nums) {
+//            sum += a;
+//        }
+//        return sum;
+//    }
 
     public static void main(String[] args) {
         int[] nums = {1,2,4,5,6,7,8,9,6,5,3,6,6,7,66,2};
