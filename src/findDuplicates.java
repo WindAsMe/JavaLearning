66ea63e96b4d3452218a365e9c5cda610d0fde0d
WindAsMe <542636539@qq.com>
2018-06-20 13:23:15 +0800

    Java For LeeCode

--- src/findDuplicates.java
+++ src/findDuplicates.java
@@ -11,17 +11,40 @@ import java.util.List;
  */
 public class findDuplicates {
 
+    // Ordinary function:
+    // After sort, comparing
+//    private static List<Integer> findDuplicatesResult(int[] nums) {
+//        List<Integer> list = new ArrayList<>();
+//        Arrays.sort(nums);
+//        for (int i = 0 ; i < nums.length - 1 ; i ++) {
+//            if (nums[i] == nums[i + 1]) {
+//                list.add(nums[i]);
+//            }
+//        }
+//        return list;
+//    }
+
+
+    // Profound function
     private static List<Integer> findDuplicatesResult(int[] nums) {
-        List<Integer> list = new ArrayList<>();
-        Arrays.sort(nums);
-        for (int i = 0 ; i < nums.length - 1 ; i ++) {
-            if (nums[i] == nums[i + 1]) {
-                list.add(nums[i]);
-            }
+        int[] arr = new int[nums.length + 1];
+
+        // Mark the occurrence times
+        for (int i : nums) {
+            arr[i]++;
         }
+
+        ArrayList<Integer> list = new ArrayList<>();
+        for (int i = 0; i < arr.length; i++) {
+            // If occurrence is 2, add to list
+            if(arr[i] > 1){
+                list.add(i);
+            }
+
+        }
+
         return list;
     }
-
     public static void main(String[] args) {
         int[] nums = {4,3,2,7,8,2,3,1};
         List<Integer> list = findDuplicatesResult(nums);
