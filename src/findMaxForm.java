c37ddf882a70bc55b3bea7970c7dd4e627b3045c
WindAsMe <542636539@qq.com>
2018-06-20 15:05:51 +0800

    Java For LeeCode

--- src/findMaxForm.java
+++ src/findMaxForm.java
@@ -11,35 +11,56 @@ import java.util.Comparator;
 public class findMaxForm {
 
     private static int findMaxFormResult(String[] strs, int m, int n) {
-        Arrays.sort(strs, new Comparator<String>() {
-            @Override
-            public int compare(String o1, String o2) {
-                return o1.length() - o2.length();
-            }
-        });
-
-        int count = 0;
+        int[][] dp = new int[m + 1][n + 1];
         for (String str : strs) {
-            int zeroCount = 0;
-            int oneCount = 0;
-            for (int j = 0; j < str.length(); j++) {
-                if ('0' == str.charAt(j)) {
-                    zeroCount++;
+            int zeros = 0, ones = 0;
+            for (int i = 0 ; i < str.length() ; i ++) {
+                if (str.charAt(i) == '0') {
+                    zeros ++;
                 } else {
-                    oneCount++;
+                    ones ++;
                 }
             }
-            if (m - zeroCount >= 0 && n - oneCount >= 0) {
-                m -= zeroCount;
-                n -= oneCount;
-                count ++;
-            } else if (m - zeroCount < 0 && n - oneCount < 0) {
-                return count;
+            for (int i = m; i >= zeros; --i) {
+                for (int j = n; j >= ones; --j) {
+                    dp[i][j] = Math.max(dp[i][j], dp[i - zeros][j - ones] + 1);
+                }
             }
         }
-        return count;
-
+        return dp[m][n];
     }
+
+    // Greedy Algorithm is not proper
+//    private static int findMaxFormResult(String[] strs, int m, int n) {
+//        Arrays.sort(strs, new Comparator<String>() {
+//            @Override
+//            public int compare(String o1, String o2) {
+//                return o1.length() - o2.length();
+//            }
+//        });
+//
+//        int count = 0;
+//        for (String str : strs) {
+//            int zeroCount = 0;
+//            int oneCount = 0;
+//            for (int j = 0; j < str.length(); j++) {
+//                if ('0' == str.charAt(j)) {
+//                    zeroCount++;
+//                } else {
+//                    oneCount++;
+//                }
+//            }
+//            if (m - zeroCount >= 0 && n - oneCount >= 0) {
+//                m -= zeroCount;
+//                n -= oneCount;
+//                count ++;
+//            } else if (m - zeroCount < 0 && n - oneCount < 0) {
+//                return count;
+//            }
+//        }
+//        return count;
+//
+//    }
 
     public static void main(String[] args) {
         String[] strs = {"111","1000","1000","1000"};
