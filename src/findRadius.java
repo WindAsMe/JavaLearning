4536f81db7225e37fd63aff9205884708c93d6f0
WindAsMe <542636539@qq.com>
2018-06-21 13:49:10 +0800

    Java For LeeCode

--- src/findRadius.java
+++ src/findRadius.java
@@ -1,3 +1,5 @@
+import java.util.Arrays;
+
 /**
  * Author     : WindAsMe
  * File       : findRadius.java
@@ -6,4 +8,35 @@
  * Function   :
  */
 public class findRadius {
+
+    private static int findRadiusResult(int[] houses, int[] heaters) {
+        Arrays.sort(houses);
+        Arrays.sort(heaters);
+        int result = 0;
+        int temp = 0;
+        int j = 0;
+        for (int house : houses) {
+            // Find the each house point
+            // The NEAREST left heater and NEAREST right heater
+            while (j < heaters.length && heaters[j] < house)
+                j ++;
+            if (j == heaters.length)
+                temp = house - heaters[j - 1];
+            else if (j == 0)
+                temp = heaters[j] - house;
+            else if (heaters[j] > house)
+                temp = Math.min(heaters[j] - house, house - heaters[j - 1]);
+            if (temp > result)
+                result = temp;
+        }
+        return result;
+    }
+
+
+    public static void main(String[] args) {
+        int[] houses = {1,2,3,4};
+        int[] heaters = {1,4};
+        System.out.println(findRadiusResult(houses, heaters));
+    }
 }
+
