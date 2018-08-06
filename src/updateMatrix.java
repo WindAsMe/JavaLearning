98c39efedce65bae85809db34ae0d00a67139e03
WindAsMe <542636539@qq.com>
2018-08-06 17:27:31 +0800

    Java For LeetCode

--- src/updateMatrix.java
+++ src/updateMatrix.java
@@ -18,16 +18,19 @@ public class updateMatrix {
         Queue<int[]> queue = new LinkedList<>();
         for (int i = 0; i < m; i++) {
             for (int j = 0; j < n; j++) {
-                if (matrix[i][j] == 0) {
+                if (matrix[i][j] == 0)
                     queue.offer(new int[] {i, j});
-                }
-                else {
+                else
                     matrix[i][j] = Integer.MAX_VALUE;
-                }
             }
         }
 
-        int[][] dirs = {{-1, 0}, {1, 0}, {0, -1}, {0, 1}};
+        int[][] dirs = {
+                {-1, 0},
+                {1, 0},
+                {0, -1},
+                {0, 1}
+        };
 
         while (!queue.isEmpty()) {
             int[] cell = queue.poll();
@@ -35,7 +38,8 @@ public class updateMatrix {
                 int r = cell[0] + d[0];
                 int c = cell[1] + d[1];
                 if (r < 0 || r >= m || c < 0 || c >= n ||
-                        matrix[r][c] <= matrix[cell[0]][cell[1]] + 1) continue;
+                        matrix[r][c] <= matrix[cell[0]][cell[1]] + 1)
+                    continue;
                 queue.add(new int[] {r, c});
                 matrix[r][c] = matrix[cell[0]][cell[1]] + 1;
             }
