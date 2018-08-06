64a7b81ad5963a09104e51f6af20fe31e4138b0c
WindAsMe <542636539@qq.com>
2018-08-06 15:54:08 +0800

    Java For LeetCode

--- src/canPartition.java
+++ src/canPartition.java
@@ -1,4 +1,5 @@
 import java.util.Arrays;
+import java.util.stream.IntStream;
 
 /**
  * Author     : WindAsMe
@@ -10,36 +11,55 @@ import java.util.Arrays;
 public class canPartition {
 
     private static boolean canPartitionResult(int[] nums) {
+        // Length < 2 || sum is minus:
+        // return false
         if (nums.length < 2)
             return false;
-        Arrays.sort(nums);
         int sum = 0;
         for (int num : nums)
             sum += num;
+        if (sum % 2 == 1)
+            return false;
+
         for (int i = 0; i < nums.length; i++) {
-            if (dfs(nums[i], sum - nums[i], i, nums, nums[i] > sum - nums[i]))
+            if (dfs(nums[i], sum - nums[i], i, nums))
                 return true;
         }
         return false;
     }
 
-    private static boolean dfs(int left, int right, int index, int[] nums, boolean bigger) {
+    private static boolean dfs(int left, int right, int index, int[] nums) {
+        System.out.println("left: " + left + " right: " + right);
         if (left == right)
             return true;
         if (index < nums.length) {
             for (int i = index + 1; i < nums.length; i++) {
-                if (bigger != left + nums[i] > right - nums[i])
-                    break;
-                if (dfs(left + nums[i], right - nums[i], i, nums, left + nums[i] > right - nums[i]))
+                if (dfs(left + nums[i], right - nums[i], i, nums))
                     return true;
             }
         }
         return false;
     }
 
+
+    private static boolean canPartitionResult1(int[] nums) {
+        int sum = 0;
+        for (int i : nums)
+            sum += i;
+        if (sum % 2 != 0)
+            return false;
+        int half = sum / 2;
+        return dfs(half, nums.length, nums);
+    }
+
+    private static boolean dfs(int w, int s, int[] nums) {
+        return w == 0 || w >= 0 && s != 0 && (dfs(w - nums[s - 1], s - 1, nums) || dfs(w, s - 1, nums));
+    }
+
+
     public static void main(String[] args) {
-        int[]  nums = {28,63,95,30,39,16,36,44,37,100,61,73,32,71,100,2,37,60,23,71,53,70,69,82,97,43,16,33,29,5,97,32,29,78,93,59,37,88,89,79,75,9,74,32,81,12,34,13,16,15,16,40,90,70,17,78,54,81,18,92,75,74,59,18,66,62,55,19,2,67,30,25,64,84,25,76,98,59,74,87,5,93,97,68,20,58,55,73,74,97,49,71,42,26,8,87,99,1,16,79};
-        System.out.println(canPartitionResult(nums));
+        int[] nums = {1,2,2,5,5,7};
+        System.out.println(canPartitionResult1(nums));
     }
 
 }
