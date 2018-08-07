9d9814e188acf034730aabe694e339a58caa47ee
WindAsMe <542636539@qq.com>
2018-08-07 14:19:17 +0800

    Java For LeetCode

---
+++ src/findFrequentTreeSum.java
@@ -0,0 +1,52 @@
+import java.util.*;
+
+/**
+ * Author     : WindAsMe
+ * File       : findFrequentTreeSum.java
+ * Time       : Create on 18-8-7
+ * Location   : ../Home/JavaForLeeCode2/findFrequentTreeSum.java
+ * Function   : LeetCode No.508
+ */
+public class findFrequentTreeSum {
+
+    private static class TreeNode {
+        int val;
+        TreeNode left;
+        TreeNode right;
+        TreeNode(int x) { val = x; }
+    }
+
+    private static int[] findFrequentTreeSumResult(TreeNode root) {
+        if (root == null)
+            return new int[0];
+        Map<Integer, Integer> map = new HashMap<>();
+        List<Integer> list = new ArrayList<>();
+        dfs(root, map);
+        int occur = 0;
+        for (Map.Entry<Integer, Integer> temp : map.entrySet())
+            occur = temp.getValue() > occur ? temp.getValue() : occur;
+        for (Map.Entry<Integer, Integer> temp : map.entrySet()) {
+            if (temp.getValue() == occur)
+                list.add(temp.getKey());
+        }
+        int[] ans = new int[list.size()];
+        for (int i = 0; i < list.size(); i++)
+            ans[i] = list.get(i);
+        return ans;
+    }
+
+    private static void dfs(TreeNode node, Map<Integer, Integer> map) {
+        if (node != null) {
+            map.merge(node.val, 1, (a, b) -> a + b);
+            dfs(node.left, map);
+            dfs(node.right, map);
+        }
+    }
+
+    public static void main(String[] args) {
+        TreeNode node = new TreeNode(2);
+        node.left = new TreeNode(2);
+        node.right = new TreeNode(5);
+        System.out.println(Arrays.toString(findFrequentTreeSumResult(node)));
+    }
+}
