0b475fd50f05890ae944b10ba04c417ab1a21034
WindAsMe <542636539@qq.com>
2018-08-07 15:28:36 +0800

    Java For LeetCode

--- src/checkSubarraySum.java
+++ src/checkSubarraySum.java
@@ -8,6 +8,7 @@
 public class checkSubarraySum {
 
     private static boolean checkSubarraySumResult(int[] nums, int k) {
+        // k == 0 must be concerned !!
         if (k == 0) {
             for (int i = 0; i < nums.length - 1; i++) {
                 if (nums[i] == 0 && nums[i + 1] == 0)
