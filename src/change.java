fb7771072fbeff5ea87de6974c41c17f55d900b6
WindAsMe <542636539@qq.com>
2018-08-07 14:58:52 +0800

    Java For LeetCode

--- src/change.java
+++ src/change.java
@@ -7,6 +7,21 @@
  */
 public class change {
 
+    // DP problem
+    private static int changeResult1(int amount, int[] coins) {
+        int[][] dp = new int[coins.length + 1][amount + 1];
+        dp[0][0] = 1;
+        for (int i = 1; i <= coins.length; ++i) {
+            dp[i][0] = 1;
+            for (int j = 1; j <= amount; ++j) {
+                dp[i][j] = dp[i - 1][j] + (j >= coins[i - 1] ? dp[i][j - coins[i - 1]] : 0);
+            }
+        }
+        return dp[coins.length][amount];
+    }
+
+
+    // Brutal proceeding: TL
     private static int changeResult(int amount, int[] coins) {
         if (amount == 0)
             return 1;
@@ -21,6 +36,7 @@ public class change {
     // Here index is important
     // If no index, the Algorithm will add the duplications
     private static void valid(int account, int target, int[] coins, int[] ans, int index) {
+        System.out.println(account);
         if (account == target) {
             ans[0]++;
             return;
@@ -32,7 +48,7 @@ public class change {
     }
 
     public static void main(String[] args) {
-        int[] coins = {1,2,5};
-        System.out.println(changeResult(5, coins));
+        int[] coins = {2};
+        System.out.println(changeResult1(500, coins));
     }
 }
