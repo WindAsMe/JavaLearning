3a59bd7b7f8f8326ba5d4725ebe0f6991cb1f535
WindAsMe <542636539@qq.com>
2018-08-08 15:30:36 +0800

    Java For LeetCode

--- src/findTilt.java
+++ src/findTilt.java
@@ -11,47 +11,26 @@ public class findTilt {
         int val;
         TreeNode left;
         TreeNode right;
-        TreeNode(int x) { val = x; }
+
+        TreeNode(int x) {
+            val = x;
+        }
     }
 
 
+    private static int res = 0;
     private static int findTiltResult(TreeNode root) {
-        int[] ans = {0};
-        dfs(root, ans);
-        return ans[0];
+        dfs(root);
+        return res;
     }
 
-    private static void add(TreeNode node, int[] sum) {
-        if (node != null) {
-            sum[0] += node.val;
-            add(node.left, sum);
-            add(node.right, sum);
-        }
-
-    }
-
-    private static void dfs(TreeNode node, int[] ans) {
-        if (node == null || (node.left == null && node.right == null))
-            return;
-        if (node.left == null) {
-            int[] sum = {0};
-            add(node.right, sum);
-            ans[0] += sum[0];
-            dfs(node.right, ans);
-        } else if (node.right == null) {
-            int[] sum = {0};
-            add(node.left, sum);
-            ans[0] += sum[0];
-            dfs(node.left, ans);
-        } else {
-            int[] sum1 = {0};
-            int[] sum2 = {0};
-            add(node.left, sum1);
-            add(node.right, sum2);
-            ans[0] += Math.abs(sum1[0] - sum2[0]);
-            dfs(node.left, ans);
-            dfs(node.right, ans);
-        }
+    private static int dfs(TreeNode node) {
+        if(node == null)
+            return 0;
+        int left = dfs(node.left);
+        int right= dfs(node.right);
+        res += Math.abs(left - right);
+        return left + right + node.val;
     }
 
     public static void main(String[] args) {
