32bfad242a7756918bc6b41ba9c75d129d6ef219
WindAsMe <542636539@qq.com>
2018-08-10 15:24:31 +0800

    Java For LeetCode

--- src/matrixScore.java
+++ src/matrixScore.java
@@ -18,12 +18,28 @@ public class matrixScore {
                     a[i] = a[i] == 1 ? 0 : 1;
             }
         }
+        for (int i = 0; i < A[0].length; i++) {
+            int zero = 0;
+            int one = 0;
+            for (int[] aA : A) {
+                if (aA[i] == 0)
+                    zero++;
+                else
+                    one++;
+            }
+            if (zero > one)
+                convert(A, i);
+        }
 
         for (int[] a : A)
             System.out.println(Arrays.toString(a));
         return 0;
     }
 
+    private static void convert(int[][] A, int index) {
+        for (int i = 0 ; i < A.length; i++)
+            A[i][index] = A[i][index] == 0 ? 1 : 0;
+    }
 
     public static void main(String[] args) {
         int[][] A = {
