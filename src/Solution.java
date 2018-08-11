b49a2c4c2c3bd2aea71aef69f192a79b0277f12d
WindAsMe <542636539@qq.com>
2018-08-11 16:10:29 +0800

    Java For LeetCode

--- src/Solution.java
+++ src/Solution.java
@@ -7,24 +7,24 @@
  */
 public class Solution {
 
-    private static int[] nums;
+    private int[] nums;
     public Solution(int[] nums) {
         this.nums = nums.clone();
     }
 
     /** Resets the array to its original configuration and return it. */
-    public static int[] reset() {
+    public int[] reset() {
         return nums;
     }
 
     /** Returns a random shuffling of the array. */
-    public static int[] shuffle() {
+    public int[] shuffle() {
         int[] ans = nums.clone();
-        for (int i = ans.length - 1 ; i > 0 ; i -- ) {
-            int temp = ans[i];
+        for (int i = 0; i < ans.length; i++) {
             int index = (int) (Math.random() * i);
-            ans[index] = temp;
-            ans[i] = ans[index];
+            int temp = nums[index];
+            nums[index] = nums[i];
+            nums[i] = temp;
         }
         return ans;
     }
