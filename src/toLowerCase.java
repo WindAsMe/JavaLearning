c77959f21b8480218168f7e37825c497ab82e5cb
WindAsMe <542636539@qq.com>
2018-08-12 09:58:42 +0800

    Java For LeetCode

---
+++ src/toLowerCase.java
@@ -0,0 +1,17 @@
+/**
+ * Author     : WindAsMe
+ * File       : toLowerCase.java
+ * Time       : Create on 18-8-12
+ * Location   : ../Home/JavaForLeeCode2/toLowerCase.java
+ * Function   : LeetCode No.709
+ */
+public class toLowerCase {
+
+    private static String toLowerCaseResult(String str) {
+        return str.toLowerCase();
+    }
+
+    public static void main(String[] args) {
+        System.out.println(toLowerCaseResult("asd"));
+    }
+}
