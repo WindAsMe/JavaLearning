8990e405b3cc868d3ca1a53e766939cc5feb8886
WindAsMe <542636539@qq.com>
2018-08-12 11:16:29 +0800

    Java For LeetCode

---
+++ src/KthLargest.java
@@ -0,0 +1,60 @@
+import java.util.ArrayList;
+import java.util.Collections;
+import java.util.Comparator;
+import java.util.List;
+
+/**
+ * Author     : WindAsMe
+ * File       : KthLargest.java
+ * Time       : Create on 18-8-12
+ * Location   : ../Home/JavaForLeeCode2/KthLargest.java
+ * Function   : LeetCode No.703
+ */
+public class KthLargest {
+
+    private static class Solution {
+        private List<Integer> list;
+        private int k;
+
+        public Solution(int k, int[] nums) {
+            this.k = k;
+            list = new ArrayList<>();
+            for (int num : nums)
+                list.add(num);
+            Collections.sort(list, new Comparator<Integer>() {
+                @Override
+                public int compare(Integer o1, Integer o2) {
+                    return o1 - o2;
+                }
+            });
+        }
+
+        public int add(int val) {
+            for (int i = 0; i < list.size(); i++) {
+                if (list.get(i) < val) {
+                    list.add(i, val);
+                    break;
+                }
+            }
+            return list.get(k);
+        }
+    }
+
+
+    public static void main(String[] args) {
+        int[] arr = {4,5,8,2};
+        Solution s = new Solution(3, arr);
+        System.out.println(s.add(3));
+        System.out.println(s.add(5));
+        System.out.println(s.add(10));
+        System.out.println(s.add(9));
+        System.out.println(s.add(4));
+
+    }
+}
+
+/**
+ * Your KthLargest object will be instantiated and called as such:
+ * KthLargest obj = new KthLargest(k, nums);
+ * int param_1 = obj.add(val);
+ */
