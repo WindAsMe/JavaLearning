14f3d48b5fd0942a277b731ab62398290a3bbdb0
WindAsMe <542636539@qq.com>
2018-08-12 10:04:57 +0800

    Java For LeetCode

--- src/MyHashMap.java
+++ src/MyHashMap.java
@@ -23,7 +23,7 @@ public class MyHashMap {
 
     /** Returns the value to which the specified key is mapped, or -1 if this map contains no mapping for the key */
     public int get(int key) {
-        return map.get(key);
+        return map.get(key) == null ? -1 : map.get(key);
     }
 
     /** Removes the mapping of the specified value key if this map contains a mapping for the key */
