38d5bb3ce04ccbe952045f6f94390b8ef2889194
WindAsMe <542636539@qq.com>
2018-08-13 16:46:46 +0800

    Java For LeetCode

---
+++ src/minimumDeleteSum.java
@@ -0,0 +1,40 @@
+import java.util.Arrays;
+
+/**
+ * Author     : WindAsMe
+ * File       : minimumDeleteSum.java
+ * Time       : Create on 18-8-13
+ * Location   : ../Home/JavaForLeeCode2/minimumDeleteSum.java
+ * Function   : LeetCode No.712
+ */
+public class minimumDeleteSum {
+
+    private static int minimumDeleteSumResult(String s1, String s2) {
+        char[] c1 = s1.toCharArray();
+        char[] c2 = s2.toCharArray();
+        int[][] ans = new int[c1.length + 1][c2.length + 1];
+        for (int i = 1; i <= c1.length; i++)
+            ans[i][0] = ans[i - 1][0] + c1[i - 1];
+        for (int i = 1; i <= c2.length; i++)
+            ans[0][i] = ans[0][i - 1] + c2[i - 1];
+
+
+        for (int i = 1; i <= c1.length; i++) {
+            for (int j = 1; j <= c2.length; j++) {
+                if (c1[i - 1] == c2[j - 1])
+                    ans[i][j] = Math.min(Math.min(ans[i - 1][j], ans[i][j - 1]), ans[i - 1][j - 1]);
+                else
+                    ans[i][j] = Math.min(Math.min(ans[i - 1][j], ans[i][j - 1]), ans[i - 1][j - 1]) + Math.min(c1[i - 1], c2[j - 1]);
+            }
+        }
+
+        for (int[] a : ans)
+            System.out.println(Arrays.toString(a));
+
+        return ans[c1.length][c2.length];
+    }
+
+    public static void main(String[] args) {
+        System.out.println(minimumDeleteSumResult("jerry", "jack"));
+    }
+}
