c68331edb70c569737f3a9e71b37c7ebdef2e806
WindAsMe <542636539@qq.com>
2018-08-13 16:11:25 +0800

    Java For LeetCode

---
+++ src/edit.java
@@ -0,0 +1,38 @@
+import java.util.Arrays;
+
+/**
+ * Author     : WindAsMe
+ * File       : edit.java
+ * Time       : Create on 18-8-13
+ * Location   : ../Home/JavaForLeeCode2/edit.java
+ * Function   : dp: Edit Distance
+ */
+public class edit {
+
+    private static int editResult(String s1, String s2) {
+        char[] c1 = s1.toCharArray();
+        char[] c2 = s2.toCharArray();
+        int[][] ans = new int[c1.length + 1][c2.length + 1];
+        for (int i = 1; i <= c1.length; i++)
+            ans[i][0] = i;
+        for (int i = 1; i <= c2.length; i++)
+            ans[0][i] = i;
+        for (int i = 1; i <= c1.length; i++) {
+            for (int j = 1; j <= c2.length; j++) {
+                if (c1[i - 1] == c2[j - 1])
+                    ans[i][j] = Math.min(Math.min(ans[i][j - 1], ans[i - 1][j]), ans[i - 1][j - 1]);
+                else
+                    ans[i][j] = Math.min(Math.min(ans[i][j - 1], ans[i - 1][j]), ans[i - 1][j - 1]) + 1;
+            }
+        }
+
+        for (int[] a : ans)
+            System.out.println(Arrays.toString(a));
+
+        return ans[c1.length - 1][c2.length - 1];
+    }
+
+    public static void main(String[] args) {
+        System.out.println(editResult("jerry", "jary"));
+    }
+}
