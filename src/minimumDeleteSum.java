5015869546ab8188af0da5e58a2aa21397b0e8ba
WindAsMe <542636539@qq.com>
2018-08-13 16:59:07 +0800

    Java For LeetCode

--- src/minimumDeleteSum.java
+++ src/minimumDeleteSum.java
@@ -13,19 +13,19 @@ public class minimumDeleteSum {
         char[] c1 = s1.toCharArray();
         char[] c2 = s2.toCharArray();
         int[][] ans = new int[c1.length + 1][c2.length + 1];
+
+        // init: use the ascii sum to init the array
         for (int i = 1; i <= c1.length; i++)
             ans[i][0] = ans[i - 1][0] + c1[i - 1];
         for (int i = 1; i <= c2.length; i++)
             ans[0][i] = ans[0][i - 1] + c2[i - 1];
-
 
         for (int i = 1; i <= c1.length; i++) {
             for (int j = 1; j <= c2.length; j++) {
                 int cost = 0;
                 if (c1[i - 1] != c2[j - 1])
                     cost = c1[i - 1] + c2[j - 1];
-                ans[i][j] = Math.min(ans[i - 1][j] + c1[i - 1], ans[i][j - 1] + c2[j - 1]);
-                ans[i][j] = Math.min(ans[i][j], ans[i - 1][j - 1] + cost);
+                ans[i][j] = Math.min(ans[i - 1][j - 1] + cost, Math.min(ans[i - 1][j] + c1[i - 1], ans[i][j - 1] + c2[j - 1]));
             }
         }
 
