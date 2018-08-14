4299a83361bf99d7993d9e7f9cecb1fd4d6e256e
WindAsMe <542636539@qq.com>
2018-08-14 14:55:44 +0800

    Java For LeetCode

--- src/maxProfit2.java
+++ src/maxProfit2.java
@@ -1,3 +1,5 @@
+import java.util.Arrays;
+
 /**
  * Author     : WindAsMe
  * File       : maxProfit2.java
@@ -13,6 +15,8 @@ public class maxProfit2 {
 
         buy[0] = -prices[0];
         for(int i = 1; i < prices.length; i++) {
+            // System.out.println("buy: " + Arrays.toString(buy));
+            // System.out.println("sell: " + Arrays.toString(sell));
             buy[i] = Math.max(buy[i - 1], sell[i - 1] - prices[i]);
             sell[i] = Math.max(sell[i - 1], buy[i - 1] + prices[i] - fee);
         }
