d003f23bf61c6563218f221d566a624a91b22ea0
WindAsMe <542636539@qq.com>
2018-08-14 14:51:50 +0800

    Java For LeetCode

--- src/maxProfit2.java
+++ src/maxProfit2.java
@@ -8,6 +8,19 @@
 public class maxProfit2 {
 
     private static int maxProfitResult(int[] prices, int fee) {
+        int[] buy = new int[prices.length + 1];
+        int[] sell = new int[prices.length + 1];
 
+        buy[0] = -prices[0];
+        for(int i = 1; i < prices.length; i++) {
+            buy[i] = Math.max(buy[i - 1], sell[i - 1] - prices[i]);
+            sell[i] = Math.max(sell[i - 1], buy[i - 1] + prices[i] - fee);
+        }
+        return sell[prices.length - 1];
+    }
+
+    public static void main(String[] args) {
+        int[] prices = {1, 3, 2, 8, 4, 9};
+        System.out.println(maxProfitResult(prices, 2));
     }
 }
