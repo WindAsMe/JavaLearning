637e05c2530384e2c39be34a4acfe9feef1de7b3
WindAsMe <542636539@qq.com>
2018-08-14 16:55:11 +0800

    Java For LeetCode

--- src/baggage.java
+++ src/baggage.java
@@ -10,12 +10,19 @@ import java.util.Arrays;
 public class baggage {
 
     private static int bag(int[] w, int[] v, int C) {
+        // The array is [Object's length + 1][Capacity + 1]
+        // Every step array[i][j] means:
+        // The Capacity is j, and you face the w[i], v[i] Object
         int[][] ans = new int[w.length + 1][C + 1];
         for (int i = 1; i <= w.length; i++) {
             for (int j = 1; j <= C; j++) {
+                // If you're lack of Capacity
                 if (j < w[i - 1])
                     ans[i][j] = ans[i - 1][j];
+                // If you're spacious
                 else
+                    // Comparing with the head or carry out the before
+                    // You can do this because every step is OPTIMUM
                     ans[i][j] = Math.max(ans[i - 1][j], ans[i - 1][j - w[i - 1]] + v[i - 1]);
             }
         }
