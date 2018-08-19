5d1989f4af94e3b266241e1ebf7fc2fd263b491a
WindAsMe <542636539@qq.com>
2018-08-19 15:26:04 +0800

    Java For LeetCode

--- src/leastInterval.java
+++ src/leastInterval.java
@@ -10,18 +10,17 @@ public class leastInterval {
 
     private static int leastIntervalResult(char[] tasks, int n) {
         int[] c = new int[26];
-        for(char t : tasks){
-            c[t - 'A']++;
-        }
-        Arrays.sort(c);
         int i = 25;
-        while(i >= 0 && c[i] == c[25]) i--;
-
+        for(char t : tasks)
+            c[t - 'A']++;
+        Arrays.sort(c);
+        while(i >= 0 && c[i] == c[25])
+            i--;
         return Math.max(tasks.length, (c[25] - 1) * (n + 1) + 25 - i);
     }
 
     public static void main(String[] args) {
         char[] task = {'B', 'B', 'B', 'A', 'A', 'A'};
-        leastIntervalResult(task, 1);
+        System.out.println(leastIntervalResult(task, 2));
     }
 }
