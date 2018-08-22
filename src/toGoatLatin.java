29b39f6670e40c9fe0056a90b933529461f24240
WindAsMe <542636539@qq.com>
2018-08-22 15:48:06 +0800

    Java For LeetCode

---
+++ src/toGoatLatin.java
@@ -0,0 +1,45 @@
+/**
+ * Author     : WindAsMe
+ * File       : toGoatLatin.java
+ * Time       : Create on 18-8-22
+ * Location   : ../Home/JavaForLeeCode2/toGoatLatin.java
+ * Function   : LeetCode No.824
+ */
+public class toGoatLatin {
+
+    private static String toGoatLatinResult(String S) {
+        S += " ";
+        char[] helper = S.toCharArray();
+        StringBuilder ans = new StringBuilder();
+        StringBuilder temp = new StringBuilder();
+        int flag = 1;
+        char reverse = '0';
+        boolean needed = false;
+        for (char c : helper) {
+            if (temp.length() == 0 && (c == 'a' || c == 'e' || c == 'i' || c == 'o' || c == 'u'))
+                temp.append(c);
+            else if (temp.length() == 0) {
+                reverse = c;
+                needed = true;
+            } else {
+                if (c == ' ') {
+                    if (needed) {
+                        temp.append(reverse);
+                        needed = false;
+                    }
+                    temp.append("ma");
+                    for (int i = 0; i < flag; i++)
+                        temp.append("a");
+                    ans.append(temp).append(" ");
+                    temp.delete(0, temp.length());
+                } else
+                    temp.append(c);
+            }
+        }
+        return ans.toString();
+    }
+
+    public static void main(String[] args) {
+        System.out.println(toGoatLatinResult("I speak Goat Latin"));
+    }
+}
