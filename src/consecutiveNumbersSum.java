193812f51f39311a8b552ce4a7b611da8f1be197
WindAsMe <542636539@qq.com>
2018-08-23 21:14:44 +0800

    Java For LeetCode

---
+++ src/consecutiveNumbersSum.java
@@ -0,0 +1,25 @@
+/**
+ * Author     : WindAsMe
+ * File       : consecutiveNumbersSum.java
+ * Time       : Create on 18-8-23
+ * Location   : ../Home/JavaForLeeCode2/consecutiveNumbersSum.java
+ * Function   : LeetCode No.829
+ */
+public class consecutiveNumbersSum {
+
+    private static int consecutiveNumbersSumResult(int N) {
+        int count = 1;
+        for (int i = N - 1; i > 0; i--) {
+            int sum = i;
+            for (int j = sum - 1; j > 0; j--)
+                sum += j;
+            if (sum == N)
+                count++;
+        }
+        return count;
+    }
+
+    public static void main(String[] args) {
+        System.out.println(consecutiveNumbersSumResult(5));
+    }
+}
