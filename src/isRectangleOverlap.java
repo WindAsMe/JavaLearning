12d4864dd4b38546a52054463cc6728ec2ac5726
WindAsMe <542636539@qq.com>
2018-08-23 21:46:56 +0800

    Java For LeetCode

--- src/isRectangleOverlap.java
+++ src/isRectangleOverlap.java
@@ -1,5 +1,3 @@
-import java.util.Arrays;
-
 /**
  * Author     : WindAsMe
  * File       : isRectangleOverlap.java
@@ -12,20 +10,12 @@ public class isRectangleOverlap {
     private static boolean isRectangleOverlapResult(int[] rec1, int[] rec2) {
         // (rec[0], rec[1]): (x1, y1)
         // (rec[2], rec[3]): (x2, y2)
-        int[] point1 = {(rec1[0] + rec1[2]) / 2, (rec1[1] + rec1[3] ) / 2};
-        int[] point2 = {(rec2[0] + rec2[2]) / 2, (rec2[1] + rec2[3] ) / 2};
-        System.out.println("point1: " + Arrays.toString(point1));
-        System.out.println("point2: " + Arrays.toString(point2));
-        double dis = Math.pow(point1[1] - point2[1], 2) + Math.pow(point1[0] - point2[0], 2);
-        double parallax = Math.pow(point1[0] - rec1[0] + point2[0] - rec2[0], 2) + Math.pow(point1[1] - rec1[1] + point2[1] - rec2[1], 2);
-        System.out.println("dis: " + dis);
-        System.out.println("parallax: " + parallax);
-        return dis < parallax;
+        return Math.max(rec1[0], rec2[0]) < Math.min(rec1[2], rec2[2]) && Math.max(rec1[1], rec2[1]) < Math.min(rec1[3], rec2[3]);
     }
 
     public static void main(String[] args) {
-        int[] rec1 = {0,0,2,2};
-        int[] rec2 = {2,2,3,3};
+        int[] rec1 = {11,12,13,13};
+        int[] rec2 = {17,1,17,19};
         System.out.println(isRectangleOverlapResult(rec1, rec2));
     }
 }
