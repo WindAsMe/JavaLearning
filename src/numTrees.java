7c2dd81a4df0774c87cd17f06eab5304014c0c3e
WindAsMe <542636539@qq.com>
2018-08-24 21:22:01 +0800

    Java For LeetCode

--- src/numTrees.java
+++ src/numTrees.java
@@ -8,13 +8,11 @@
 public class numTrees {
 
     private static int numTreesResult(int n) {
-        if(n < 1){
+        if(n < 1)
             return 1;
-        }
         int sum = 0;
-        for(int i=1 ; i <= n ; i ++ ) {
-            sum += numTreesResult(i - 1) * numTreesResult( n - i);
-        }
+        for(int i = 1; i <= n; i++)
+            sum += numTreesResult(i - 1) * numTreesResult(n - i);
         return sum;
     }
 
