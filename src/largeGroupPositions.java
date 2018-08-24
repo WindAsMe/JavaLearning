fdbe4be5684ec63d0ce19b8a55b947af00290412
WindAsMe <542636539@qq.com>
2018-08-24 21:13:54 +0800

    Java For LeetCode

--- src/largeGroupPositions.java
+++ src/largeGroupPositions.java
@@ -17,10 +17,11 @@ public class largeGroupPositions {
         int start = 0;
         int end = 0;
         for (int i = 1; i < s.length; i++) {
+            // System.out.println("start: " + start + " end: " + end);
             if (s[i] == s[i - 1])
                 end++;
             else {
-                if (end - start >= 3) {
+                if (end - start >= 2) {
                     List<Integer> list = new ArrayList<>();
                     list.add(start);
                     list.add(end);
@@ -34,7 +35,7 @@ public class largeGroupPositions {
     }
 
     public static void main(String[] args) {
-        String s = "abbxxxxzzy";
+        String s = "abcdddeeeeaabbbcd";
         List<List<Integer>> lists = largeGroupPositionsResult(s);
         for (List<Integer> list : lists)
             System.out.println(list.toString());
