f6b1bbdf25151479143ca273904973f74ab9da6b
WindAsMe <542636539@qq.com>
2018-08-27 15:25:15 +0800

    Java For LeetCode

---
+++ src/findLongestChain.java
@@ -0,0 +1,34 @@
+/**
+ * Author     : WindAsMe
+ * File       : findLongestChain.java
+ * Time       : Create on 18-8-27
+ * Location   : ../Home/JavaForLeeCode2/findLongestChain.java
+ * Function   : LeetCode No.646
+ */
+public class findLongestChain {
+
+    private static int findLongestChainResult(int[][] pairs) {
+        int[] max = {0};
+        for (int[] pair : pairs)
+            dfs(pairs, max, pair[1], 1);
+        return max[0];
+    }
+
+    private static void dfs(int[][] pairs, int[] max, int end, int len) {
+        for (int[] pair : pairs) {
+            if (end < pair[0]) {
+                max[0] = max[0] > len + 1 ? max[0] : len + 1;
+                dfs(pairs, max, pair[1], len + 1);
+            }
+        }
+    }
+
+    public static void main(String[] args) {
+        int[][] pairs = {
+                {1, 2},
+                {2, 3},
+                {3, 4}
+        };
+        System.out.println(findLongestChainResult(pairs));
+    }
+}
