45e520c2778d2195aa3d487c808397d4149a9d8d
WindAsMe <542636539@qq.com>
2018-08-27 15:44:17 +0800

    Java For LeetCode

--- src/findLongestChain.java
+++ src/findLongestChain.java
@@ -1,3 +1,6 @@
+import java.util.Arrays;
+import java.util.Comparator;
+
 /**
  * Author     : WindAsMe
  * File       : findLongestChain.java
@@ -6,6 +9,26 @@
  * Function   : LeetCode No.646
  */
 public class findLongestChain {
+
+    // DP
+    private static int findLongestChainResult1(int[][] pairs) {
+        int[] ans = new int[pairs.length];
+        int max = 1;
+        ans[0] = 1;
+        // Must be sorted by the pair[0]
+        Arrays.sort(pairs, Comparator.comparingInt(o -> o[0]));
+        for (int i = 1; i < pairs.length; i++) {
+            for (int j = 0; j < i; j++) {
+                if (pairs[i][0] > pairs[j][1])
+                    ans[i] = ans[i] > ans[j] + 1 ? ans[i] : ans[j] + 1;
+            }
+            if (ans[i] == 0)
+                ans[i] = 1;
+            max = max > ans[i] ? max : ans[i];
+        }
+        System.out.println(Arrays.toString(ans));
+        return max;
+    }
 
     // DFS: TL
     private static int findLongestChainResult(int[][] pairs) {
@@ -25,11 +48,19 @@ public class findLongestChain {
     }
 
     public static void main(String[] args) {
+        int[][] a = {{3,4},{2,3},{1,2}};
         int[][] pairs = {
-                {1, 2},
-                {2, 3},
-                {3, 4}
+                {1,2},
+                {2,3},
+                {3,4},
+                {1,3},
+                {4,5},
+                {6,7},
+                {1,9},
+                {7,8},
+                {5,8},
+                {9,11}
         };
-        System.out.println(findLongestChainResult(pairs));
+        System.out.println(findLongestChainResult1(a));
     }
 }
