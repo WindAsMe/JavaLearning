64dc762fbf5928dee59f849ad6dc18570c15da76
WindAsMe <542636539@qq.com>
2018-08-28 16:05:04 +0800

    Java For LeetCode

--- src/printTree.java
+++ src/printTree.java
@@ -1,3 +1,4 @@
+import java.util.ArrayList;
 import java.util.List;
 
 /**
@@ -18,9 +19,13 @@ public class printTree {
 
     private static List<List<String>> printTreeResult(TreeNode root) {
         int[] depth = {0};
-        dfs(root, depth, 1);
-        System.out.println(depth[0]);
-        return null;
+        int column = 0;
+        dfs(root, depth, 0);
+        for (int i = 1; i < depth[0]; i++)
+            column = column * 2 + 1;
+        List<List<String>> lists = new ArrayList<>();
+        draw(root, 0, column, lists, column * 2 + 1);
+        return lists;
     }
 
     private static void dfs(TreeNode root, int[] depth, int cur) {
@@ -32,6 +37,14 @@ public class printTree {
         }
     }
 
+    private static void draw(TreeNode node, int layer, int column, List<List<String>> lists, int total) {
+        if (node != null) {
+            lists.get(layer).add(column, String.valueOf(node.val));
+            draw(node.left, layer + 1, column / 2, lists, total);
+            draw(node.right, layer + 1, (total - column) / 2 + 1 + column, lists, total);
+        }
+    }
+
     public static void main(String[] args) {
         TreeNode root = new TreeNode(1);
         root.left = new TreeNode(2);
@@ -39,6 +52,8 @@ public class printTree {
         root.left.left.left = new TreeNode(4);
 
         root.right = new TreeNode(5);
-        System.out.println(printTreeResult(root));
+        List<List<String>> lists = printTreeResult(root);
+        for (List<String> list : lists)
+            System.out.println(list.toString());
     }
 }
