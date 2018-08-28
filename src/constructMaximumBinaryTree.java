74ad9e16cc87f2e24805308254d9a401457b8274
WindAsMe <542636539@qq.com>
2018-08-28 15:25:07 +0800

    Java For LeetCode

--- src/constructMaximumBinaryTree.java
+++ src/constructMaximumBinaryTree.java
@@ -24,17 +24,20 @@ public class constructMaximumBinaryTree {
             }
         }
         TreeNode root = new TreeNode(max);
-        originate(0, index, nums, root.left);
-        originate(index + 1, nums.length, nums, root.right);
+          originate(0, index, nums, root, true);
+        originate(index + 1, nums.length, nums, root, false);
         return root;
     }
 
     // In recursion to originate
-    private static void originate(int start, int end, int[] nums, TreeNode node) {
+    private static void originate(int start, int end, int[] nums, TreeNode node, boolean left) {
         if (end <= start)
             return;
         if (end - start == 1) {
-            node = new TreeNode(nums[start]);
+            if (left)
+                node.left = new TreeNode(nums[start]);
+            else
+                node.right = new TreeNode(nums[start]);
             return;
         }
         int max = -1;
@@ -45,9 +48,16 @@ public class constructMaximumBinaryTree {
                 index = i;
             }
         }
-        node = new TreeNode(max);
-        originate(start, index, nums, node.left);
-        originate(index + 1, end, nums, node.right);
+        if (left) {
+            node.left = new TreeNode(max);
+            originate(start, index, nums, node.left, true);
+            originate(index + 1, end, nums, node.left, false);
+        }
+        else {
+            node.right = new TreeNode(max);
+            originate(start, index, nums, node.right, true);
+            originate(index + 1, end, nums, node.right, false);
+        }
     }
 
     private static void dfs(TreeNode node) {
