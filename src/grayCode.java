c931317d5d0db805b05333ef4f058958ff68ea05
WindAsMe <542636539@qq.com>
2018-08-31 08:47:54 +0800

    Java For LeetCode

--- src/grayCode.java
+++ src/grayCode.java
@@ -10,6 +10,10 @@ import java.util.List;
  */
 public class grayCode {
 
+    // Gray encode:
+    // b'0101: the code must be in 4 bit(firstly add 0)
+    // if i and i + 1:
+    // b'0101: num[0] != num[1], gray[0] = 1
     private static List<Integer> grayCodeResult(int n) {
         List<Integer> list = new ArrayList<>();
         for(int i = 0; i < (1 << n); i++)
