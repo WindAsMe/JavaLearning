69e45595c2ce7b483d63a2e9a4f6f0ce402a8f5c
WindAsMe <542636539@qq.com>
2018-09-02 13:38:09 +0800

    Java For LeetCode

--- src/numDistinct.java
+++ src/numDistinct.java
@@ -18,6 +18,8 @@ public class numDistinct {
             ans[0][i] = 1;
         for (int i = 1; i < tHelper.length + 1; i++) {
             for (int j = 1; j < sHelper.length + 1;  j++)
+                // If matched:
+                // add the ans[i - 1][j - 1]
                 ans[i][j] = ans[i][j - 1] + (sHelper[j - 1] == tHelper[i - 1] ? ans[i - 1][j - 1] : 0);
         }
         return ans[tHelper.length][sHelper.length];
