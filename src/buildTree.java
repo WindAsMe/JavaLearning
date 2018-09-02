751164fc0b3540658a75eed4461b6195b2a62ebb
WindAsMe <542636539@qq.com>
2018-09-02 12:58:16 +0800

    Java For LeetCode

--- src/buildTree.java
+++ src/buildTree.java
@@ -15,21 +15,31 @@ public class buildTree {
     }
 
     private static TreeNode buildTreeResult(int[] preorder, int[] inorder) {
+        if (preorder.length != inorder.length || preorder.length == 0)
+            return null;
         return build(0, 0, inorder.length - 1, preorder, inorder);
     }
+
     private static TreeNode build(int prestart,int instart,int inend,int[] preorder,int[] inorder){
-        if(prestart>preorder.length-1||instart>inend)
+        if(prestart > preorder.length - 1 || instart > inend)
             return null;
-        TreeNode root=new TreeNode(preorder[prestart]);
-        int inindex=0;
-        for(int i=instart;i<=inend;i++){
-            if(inorder[i]==root.val){
-                inindex=i;
+        TreeNode root = new TreeNode(preorder[prestart]);
+        int inindex = 0;
+        for (int i = instart; i <= inend; i++){
+            if(inorder[i] == root.val){
+                inindex = i;
                 break;
             }
         }
-        root.left=build(prestart+1,instart,inindex-1,preorder,inorder);
-        root.right=build(prestart+inindex-instart+1,inindex+1,inend,preorder,inorder);
+        root.left = build(prestart + 1, instart, inindex - 1, preorder, inorder);
+        root.right = build(prestart + inindex - instart + 1, inindex + 1, inend, preorder, inorder);
         return root;
     }
+
+    public static void main(String[] args) {
+        int[] preorder = {3,9,20,15,7};
+        int[] inorder = {9,3,15,20,7};
+
+    }
+
 }
