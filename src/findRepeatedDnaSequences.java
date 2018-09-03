93df9d64561cca18445000343bd795b06dac70b7
WindAsMe <542636539@qq.com>
2018-09-03 13:25:56 +0800

    Java For LeetCode

--- src/findRepeatedDnaSequences.java
+++ src/findRepeatedDnaSequences.java
@@ -9,20 +9,56 @@ import java.util.*;
  */
 public class findRepeatedDnaSequences {
 
+    private static List<String> findRepeatedDnaSequencesResult1(String s) {
+        List<String> ans = new ArrayList<>();
+        if(s == null || s.length() < 20)
+            return ans;
+
+        int hash = 0;
+
+        Map<Character, Integer> map = new HashMap<>();
+        map.put('A', 0);
+        map.put('C', 1);
+        map.put('G', 2);
+        map.put('T', 3);
+
+        Set<Integer> set = new HashSet<>();
+        Set<Integer> unique = new HashSet<>();
+
+        for(int i = 0; i < s.length(); i++) {
+            char c = s.charAt(i);
+            if(i < 9) {
+                hash = (hash << 2) + map.get(c);
+            } else {
+                hash = (hash << 2) + map.get(c);
+                hash &= (1 << 20) - 1;
+                if(set.contains(hash) && !unique.contains(hash)) {
+                    ans.add(s.substring(i-9, i+1));
+                    unique.add(hash);
+                } else
+                    set.add(hash);
+            }
+        }
+        return ans;
+    }
+
+
+    // TL
     private static List<String> findRepeatedDnaSequencesResult(String s) {
         if (s.length() < 20)
             return new ArrayList<>();
         char[] helper = s.toCharArray();
         List<String> list = new ArrayList<>();
         for (int i = 0; i < helper.length - 10; i++) {
-            if (valid(i, helper))
-                list.add(s.substring(i, i + 10));
+            String temp = s.substring(i, i + 10);
+            if (valid(i, helper) && !list.contains(temp))
+                list.add(temp);
         }
         return list;
     }
 
     private static boolean valid(int start, char[] helper) {
-        for (int i = start + 10; i < helper.length; i++) {
+        for (int i = start + 10; i < helper.length - 10; i++) {
             if (equal(start, i, helper))
                 return true;
         }
@@ -38,6 +74,6 @@ public class findRepeatedDnaSequences {
     }
 
     public static void main(String[] args) {
-        System.out.println(findRepeatedDnaSequencesResult("AAAAACCCCCAAAAACCCCCCAAAAAGGGTTT").toString());
+        System.out.println(findRepeatedDnaSequencesResult("AAGATCCGTCCCCCCAAGATCCGTC").toString());
     }
 }
