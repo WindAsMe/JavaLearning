bf5ea1aef3bd39c14c119ac024244b43defc079d
WindAsMe <542636539@qq.com>
2018-09-04 14:50:27 +0800

    Java For LeetCode

--- src/findOrder.java
+++ src/findOrder.java
@@ -25,7 +25,11 @@ public class findOrder {
         int[] ans = new int[numCourses];
         for (Integer i : list)
             dfs(i, ans, new ArrayList<>(), helper, prerequisites);
-        return ans;
+        for (int i : ans) {
+            if (i != 0)
+                return ans;
+        }
+        return new int[0];
     }
 
     private static void dfs(int start, int[] ans, List<Integer> list, int[] helper, int[][] prerequisites) {
@@ -57,10 +61,11 @@ public class findOrder {
     public static void main(String[] args) {
         int[][] pre = {
                 {1,0},
-                {2,0},
-                {3,1},
-                {3,2}
+                {0, 1}
+//                {2,0},
+//                {3,1},
+//                {3,2}
         };
-        System.out.println(Arrays.toString(findOrderResult(4, pre)));
+        System.out.println(Arrays.toString(findOrderResult(2, pre)));
     }
 }
