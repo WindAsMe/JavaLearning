29dd5faf4ddef3fdc77c0f6b1609d53ebafce208
WindAsMe <542636539@qq.com>
2018-09-05 18:59:03 +0800

    Java For LeetCode

--- src/firstBadVersion.java
+++ src/firstBadVersion.java
@@ -6,4 +6,31 @@
  * Function   : LeetCode No.278
  */
 public class firstBadVersion {
+
+    private static int i = 10;
+
+    private static boolean isBadVersion(int version) {
+        return version == i;
+    }
+
+    private static int firstBadVersionResult(int n) {
+        if (isBadVersion(1))
+            return 1;
+        int l = 1;
+        int r = n;
+        int mid;
+        while (l < r) {
+            mid = l / 2 + r / 2;
+            if (isBadVersion(mid))
+                r = mid;
+            else
+                l = mid + 1;
+        }
+        return l;
+    }
+
+    public static void main(String[] args) {
+        System.out.println(firstBadVersionResult(15));
+    }
+
 }
