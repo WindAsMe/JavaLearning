8c2d784bf6c724b2d4c0b11795b27f52849000bf
WindAsMe <542636539@qq.com>
2018-09-05 17:14:22 +0800

    Java For LeetCode

---
+++ src/deleteNode.java
@@ -0,0 +1,23 @@
+/**
+ * Author     : WindAsMe
+ * File       : deleteNode.java
+ * Time       : Create on 18-9-5
+ * Location   : ../Home/JavaForLeeCode2/deleteNode.java
+ * Function   :
+ */
+public class deleteNode {
+
+    private static class ListNode {
+        int val;
+        ListNode next;
+        ListNode(int x) { val = x; }
+    }
+
+    private static void deleteNodeResult(ListNode node) {
+        node.val= node.next.val;
+        node.next= node.next.next;
+    }
+
+    public static void main(String[] args) {
+    }
+}
