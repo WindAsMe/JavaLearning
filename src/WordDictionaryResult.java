2785019030b828c54a072dd8867baf4cb22e2f02
WindAsMe <542636539@qq.com>
2018-09-06 12:12:23 +0800

    Java For LeetCode

---
+++ src/WordDictionaryResult.java
@@ -0,0 +1,54 @@
+import java.util.HashSet;
+import java.util.Set;
+
+/**
+ * Author     : WindAsMe
+ * File       : WordDictionaryResult.java
+ * Time       : Create on 18-9-6
+ * Location   : ../Home/JavaForLeeCode2/WordDictionaryResult.java
+ * Function   : LeetCode No.211
+ */
+public class WordDictionaryResult {
+
+    static class WordDictionary {
+
+        private Set<String> set;
+
+        /** Initialize your data structure here. */
+        public WordDictionary() {
+            set = new HashSet<>();
+        }
+
+        /** Adds a word into the data structure. */
+        public void addWord(String word) {
+            set.add(word);
+        }
+
+        /** Returns if the word is in the data structure. A word could contain the dot character '.' to represent any one letter. */
+        public boolean search(String word) {
+            char[] help = word.toCharArray();
+            for (int i = 0; i < help.length; i++) {
+                if (i == '.') {
+                    StringBuilder s = new StringBuilder(word.substring(0, i));
+                    for (char c = 'a'; c <= 'z'; c++) {
+                        s.append(c).append(word.substring(i + 1, help.length));
+                        if (search(s.toString()))
+                            return true;
+                    }
+                }
+            }
+            return set.contains(word);
+        }
+    }
+
+    public static void main(String[] args) {
+
+    }
+
+/**
+ * Your WordDictionary object will be instantiated and called as such:
+ * WordDictionary obj = new WordDictionary();
+ * obj.addWord(word);
+ * boolean param_2 = obj.search(word);
+ */
+}
