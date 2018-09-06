a162e44fe901bf654331d5bfaffa26a3e41feefd
WindAsMe <542636539@qq.com>
2018-09-06 14:28:14 +0800

    Java For LeetCode

--- src/toHex.java
+++ src/toHex.java
@@ -8,15 +8,15 @@
 public class toHex {
 
     private static String toHexResult(int num) {
-        StringBuilder hexString = new StringBuilder();
-        char hexChar[] = {'0', '1', '2', '3', '4', '5', '6', '7', '8', '9', 'a', 'b', 'c' ,'d', 'e', 'f'};
-
-        while (num >= 0) {
-            hexString.insert(0, hexChar[num & 0xF]);
-            num = num >> 4;
+        if(num == 0)
+            return "0";
+        char map[] = {'0', '1', '2', '3', '4', '5', '6', '7', '8', '9', 'a', 'b', 'c' ,'d', 'e', 'f'};
+        StringBuilder result = new StringBuilder();
+        while(num != 0){
+            result.insert(0, map[(num & 0xF)]);
+            num = (num >>> 4);
         }
-
-        return hexString.toString().isEmpty() ? "0" : hexString.toString();
+        return result.toString();
     }
 
     public static void main(String[] args) {
