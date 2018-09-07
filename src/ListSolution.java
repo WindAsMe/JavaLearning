e0b1c280eddc13647149d488e1c944e0f43f60b5
WindAsMe <542636539@qq.com>
2018-09-07 13:02:44 +0800

    Java For LeetCode

--- src/ListSolution.java
+++ src/ListSolution.java
@@ -3,27 +3,52 @@
  * File       : ListSolution.java
  * Time       : Create on 18-9-7
  * Location   : ../Home/JavaForLeeCode2/ListSolution.java
- * Function   :
+ * Function   : LeetCode No.382
  */
 public class ListSolution {
 
-    public class ListNode {
+    public static class ListNode {
         int val;
         ListNode next;
         ListNode(int x) { val = x; }
     }
 
-    class Solution {
+    static class Solution {
 
         /** @param head The linked list's head.
         Note that the head is guaranteed to be not null, so it contains at least one node. */
-        public Solution(ListNode head) {
+        private ListNode head;
+        private int length;
 
+        public Solution(ListNode head) {
+            ListNode node = head;
+            int len = 0;
+            while (node != null) {
+                len++;
+                node = node.next;
+            }
+            this.length = len;
+            this.head = head;
         }
 
         /** Returns a random node's value. */
         public int getRandom() {
-
+            ListNode node = head;
+            int ran = (int)(Math.random() * length);
+            while (ran != 0) {
+                node = node.next;
+                ran--;
+            }
+            return node.val;
         }
     }
+
+    public static void main(String[] args) {
+        ListNode head = new ListNode(1);
+        head.next = new ListNode(2);
+        head.next.next = new ListNode(3);
+        Solution s = new Solution(head);
+        System.out.println(s.getRandom());
+    }
+
 }
