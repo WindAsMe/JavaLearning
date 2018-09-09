eceef9f856d74db87389e91cab6d80136a18e0d4
WindAsMe <542636539@qq.com>
2018-09-09 13:07:03 +0800

    Java For LeetCode

--- src/originalDigits.java
+++ src/originalDigits.java
@@ -17,16 +17,26 @@ public class originalDigits {
         StringBuilder sb = new StringBuilder();
         for (char c : helper)
             map.merge(c, 1, (a, b) -> a + b);
-        nums[8] = map.get('g');
-        nums[6] = map.get('x');
-        nums[4] = map.get('u');
-        nums[2] = map.get('w');
-        nums[0] = map.get('z');
-        nums[5] = map.get('f') - nums[4] ;
-        nums[3] = map.get('h') - nums[8] ;
-        nums[7] = map.get('s') - nums[6] ;
-        nums[1] = map.get('o') - nums[0] - nums[2] - nums[4] ;
-        nums[9] = map.get('i') - nums[5] - nums[6] - nums[8] ;
+        if (map.get('g') != null)
+            nums[8] = map.get('g');
+        if (map.get('x') != null)
+            nums[6] = map.get('x');
+        if (map.get('u') != null)
+            nums[4] = map.get('u');
+        if (map.get('w') != null)
+            nums[2] = map.get('w');
+        if (map.get('z') != null)
+            nums[0] = map.get('z');
+        if (map.get('f') != null)
+            nums[5] = map.get('f') - nums[4] ;
+        if (map.get('h') != null)
+            nums[3] = map.get('h') - nums[8] ;
+        if (map.get('s') != null)
+            nums[7] = map.get('s') - nums[6] ;
+        if (map.get('o') != null)
+            nums[1] = map.get('o') - nums[0] - nums[2] - nums[4] ;
+        if (map.get('i') != null)
+            nums[9] = map.get('i') - nums[5] - nums[6] - nums[8] ;
         for ( int i = 0 ; i < 10 ; i ++ ) {
             while (nums[i] > 0 ) {
                 sb.append(String.valueOf(i));
