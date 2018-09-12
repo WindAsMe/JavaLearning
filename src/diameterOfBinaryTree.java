8c2abfc847325099c15bf6021ac55a8db53f5fd0
WindAsMe <542636539@qq.com>
2018-09-12 13:07:03 +0800

    Java For LeetCode

---
+++ src/diameterOfBinaryTree.java
@@ -0,0 +1,39 @@
+/**
+ * Author     : WindAsMe
+ * File       : diameterOfBinaryTree.java
+ * Time       : Create on 18-9-12
+ * Location   : ../Home/JavaForLeeCode2/diameterOfBinaryTree.java
+ * Function   :
+ */
+public class diameterOfBinaryTree {
+
+    private static class TreeNode {
+        int val;
+        TreeNode left;
+        TreeNode right;
+        TreeNode(int x) { val = x; }
+    }
+
+    private static int diameterOfBinaryTreeResult(TreeNode root) {
+        if (root == null)
+            return 0;
+        return Math.max(Math.max(diameterOfBinaryTreeResult(root.left), diameterOfBinaryTreeResult(root.right)), diameterOfBinaryTreeResult(root));
+    }
+
+    private static int nodeDiameter(TreeNode node) {
+        if (node == null)
+            return 0;
+        int sum = 0;
+        if (node.left != null)
+            sum += height(node.left);
+        if (node.right != null)
+            sum += height(node.right);
+        return sum;
+    }
+
+    private static int height(TreeNode node) {
+        if (node == null)
+            return 0;
+        return 1 + Math.max(height(node.left), height(node.right));
+    }
+}
