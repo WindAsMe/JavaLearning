774caf4672b9a59e30d143a34d5e6a08f0427a5f
WindAsMe <542636539@qq.com>
2018-09-13 14:47:56 +0800

    Java For LeetCode

--- src/findPeakElement.java
+++ src/findPeakElement.java
@@ -9,34 +9,30 @@ public class findPeakElement {
 
     private static int findPeakElementResult(int[] nums) {
 
-        if (nums.length == 1){
+        if (nums.length == 1)
             return nums[0];
-        } else if (nums.length == 2){
+        else if (nums.length == 2)
             return nums[0] > nums[1] ? nums[0] : nums[1];
-        } else {
+        else {
             // Different between odd and even
             int lPointer = nums.length / 2 - ((nums.length + 1) % 2);
             int rPointer = nums.length / 2;
-            while (lPointer > 0 && rPointer < nums.length - 1){
+            while (lPointer > 0 && rPointer < nums.length - 1) {
                 System.out.println("l: " + lPointer + " " + "r: " + rPointer);
-                if (nums[lPointer] > nums[lPointer + 1] && nums[lPointer] > nums[lPointer - 1]) {
+                if (nums[lPointer] > nums[lPointer + 1] && nums[lPointer] > nums[lPointer - 1])
                     return lPointer;
-                } else {
+                else
                     lPointer -= 1;
-                }
 
-                if (nums[rPointer] > nums[rPointer + 1] && nums[rPointer] > nums[rPointer - 1]) {
+                if (nums[rPointer] > nums[rPointer + 1] && nums[rPointer] > nums[rPointer - 1])
                     return rPointer;
-                } else {
+                 else
                     rPointer += 1;
-                }
             }
-            if (nums[0] > nums[1]){
+            if (nums[0] > nums[1])
                 return 0;
-            }
-            if (nums[nums.length - 1] > nums[nums.length - 2]){
+            if (nums[nums.length - 1] > nums[nums.length - 2])
                 return nums.length - 1;
-            }
         }
 
         return 0;
