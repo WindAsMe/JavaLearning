e0efd8d3c6cab58d24ef7ebd0756b4d1fe327fe7
WindAsMe <542636539@qq.com>
2018-09-13 14:02:28 +0800

    Java For LeetCode

---
+++ src/CodecResult.java
@@ -0,0 +1,45 @@
+ /**
+ * Author     : WindAsMe
+ * File       : CodecResult.java
+ * Time       : Create on 18-9-13
+ * Location   : ../Home/JavaForLeeCode2/CodecResult.java
+ * Function   : LeetCode No.535
+ */
+public class CodecResult {
+
+    public static class Codec {
+
+        private String pre;
+        private char[] factor = {'A','B', 'C', 'D', 'E', 'a', 'b', 'c', 'd', 'e', '0', '1', '2', '3', '4'};
+        private StringBuilder ans;
+        // Encodes a URL to a shortened URL.
+        public String encode(String longUrl) {
+            char[] helper = longUrl.toCharArray();
+            StringBuilder post = new StringBuilder();
+            for (int i = 0; i < 6; i++)
+                post.append(factor[(int) (Math.random() * 15)]);
+
+            int mid = longUrl.length() - 1;
+            for (; mid >= 0; mid++) {
+                if (helper[mid] == '/')
+                    break;
+            }
+            pre = longUrl.substring(0, mid);
+            ans = new StringBuilder("http://" + longUrl.substring(mid, longUrl.length()) + post);
+            return ans.toString();
+        }
+
+        // Decodes a shortened URL to its original URL.
+        public String decode(String shortUrl) {
+            return pre + ans.substring(7, ans.length() - 6);
+        }
+    }
+
+// Your Codec object will be instantiated and called as such:
+// Codec codec = new Codec();
+// codec.decode(codec.encode(url));
+     public static void main(String[] args) {
+        Codec c = new Codec();
+         System.out.println(c.decode("https://leetcode.com/problems/design-tinyurl"));
+     }
+}
