9eff8a0eb01a6f677ba6e260965477e33e04e502
WindAsMe <542636539@qq.com>
2018-09-17 12:50:57 +0800

    Java For LeetCode

--- src/minSteps.java
+++ src/minSteps.java
@@ -13,9 +13,8 @@ public class minSteps {
         int[] dp = new int[n + 1];
         for (int i = 2; i <= n; ++i)
             dp[i] = i;
-
         for (int i = 4; i <= n; ++i) {
-            for (int j = i/2; j >= 2; --j) {
+            for (int j = i / 2; j >= 2; --j) {
                 if (i % j == 0) {
                     dp[i] = Math.min(dp[i], dp[j] + i / j);
                     break;
