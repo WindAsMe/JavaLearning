de3bd0a84a049be2998044799c7e998fed9a6159
WindAsMe <542636539@qq.com>
2018-09-20 13:18:14 +0800

    Java For LeetCode

--- src/shortestCompletingWord.java
+++ src/shortestCompletingWord.java
@@ -1,4 +1,7 @@
 import java.util.Arrays;
+import java.util.Comparator;
+import java.util.HashMap;
+import java.util.Map;
 
 /**
  * Author     : WindAsMe
@@ -10,8 +13,7 @@ import java.util.Arrays;
 public class shortestCompletingWord {
 
     private static String shortestCompletingWordResult(String licensePlate, String[] words) {
-        Arrays.sort(words);
-        System.out.println(Arrays.toString(words));
+        Arrays.sort(words, Comparator.comparingInt(String::length));
         String temp = licensePlate.toLowerCase();
         char[] cs = temp.toCharArray();
         StringBuilder builder = new StringBuilder();
@@ -19,13 +21,36 @@ public class shortestCompletingWord {
             if (c >= 'a' && c <= 'z')
                 builder.append(c);
         }
-        System.out.println(builder);
+        Map<Character, Integer> map1 = new HashMap<>();
+        for (int i = 0; i < builder.length(); i++) {
+            if (map1.get(builder.charAt(i)) == null)
+                map1.put(builder.charAt(i), 1);
+            else
+                map1.put(builder.charAt(i), map1.get(builder.charAt(i)) + 1);
+        }
+        for (String s : words) {
+            Map<Character, Integer> map2 = new HashMap<>();
+            char[] c = s.toCharArray();
+            for (char aC : c)
+                map2.merge(aC, 1, (a, b) -> a + b);
+            if (contain(map1, map2))
+                return s;
+        }
         return null;
+    }
+
+    private static boolean contain(Map<Character, Integer> m1, Map<Character, Integer> m2) {
+        System.out.println(m2.toString());
+        for (Map.Entry<Character, Integer> entry : m1.entrySet()) {
+            if (!(m2.get(entry.getKey()) != null && m2.get(entry.getKey()) >= entry.getValue()))
+                return false;
+        }
+        return true;
     }
 
     public static void main(String[] args) {
         String licensePlate = "1s3 456";
         String[] words = {"looks", "pest", "stew", "show"};
-        shortestCompletingWordResult(licensePlate, words);
+        System.out.println(shortestCompletingWordResult(licensePlate, words));
     }
 }
