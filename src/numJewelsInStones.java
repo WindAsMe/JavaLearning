76b39264c0a633592a68f5c48c72c645e9e21c4b
WindAsMe <542636539@qq.com>
2018-09-21 12:51:21 +0800

    Java For LeetCode

--- src/numJewelsInStones.java
+++ src/numJewelsInStones.java
@@ -1,13 +1,32 @@
+import java.util.HashSet;
+import java.util.Set;
+
 /**
  * Author     : WindAsMe
  * File       : numJewelsInStones.java
  * Time       : Create on 18-9-21
  * Location   : ../Home/JavaForLeeCode2/numJewelsInStones.java
- * Function   :
+ * Function   : LeetCode No.771
  */
 public class numJewelsInStones {
 
-    public int numJewelsInStones(String J, String S) {
+    private static int numJewelsInStonesResult(String J, String S) {
+        if (J.length() == 0 || S.length() == 0)
+            return 0;
+        int count = 0;
+        char[] jHelper = J.toCharArray();
+        char[] sHelper = S.toCharArray();
+        Set<Character> set = new HashSet<>();
+        for (char j : jHelper)
+            set.add(j);
+        for (char s : sHelper) {
+            if (set.contains(s))
+                count++;
+        }
+        return count;
+    }
 
+    public static void main(String[] args) {
+        System.out.println(numJewelsInStonesResult("aA", "aAAbbbb"));
     }
 }
