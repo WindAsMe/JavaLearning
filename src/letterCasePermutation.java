76abcc2fb95401f0a4ab5eca203ea207bf909271
WindAsMe <542636539@qq.com>
2018-10-05 12:46:15 +0800

    Java For LeetCode

--- src/letterCasePermutation.java
+++ src/letterCasePermutation.java
@@ -12,28 +12,24 @@ public class letterCasePermutation {
 
     private static List<String> letterCasePermutationResult(String S) {
         List<String> list = new ArrayList<>();
-        create(list, S, new StringBuilder(), S.length(), 0);
+        dfs(0, new StringBuilder(S), list);
         return list;
     }
 
-    private static void create(List<String> list, String s, StringBuilder builder, int len, int index) {
-        if (index + 1 > len) {
-            list.add(builder.toString());
+    private static void dfs(int x, StringBuilder s, List<String> list) {
+        if (x == s.length()) {
+            list.add(s.toString());
             return;
         }
-        for (int i = index + 1; i < len; i++) {
-            char c = s.charAt(i);
-            if (c >= 'a' && c <= 'z') {
-                StringBuilder temp = new StringBuilder(builder);
-                temp.append(c - 32);
-                create(list, s, temp, len, i);
-            } else if (c >= 'A' && c <= 'Z') {
-                StringBuilder temp = new StringBuilder(builder);
-                temp.append(c + 32);
-                create(list, s, temp, len, i);
-            } else
-                builder.append(c);
+        char tmp = s.charAt(x);
+        if (Character.isLetter(tmp)) {
+            s.setCharAt(x, Character.toLowerCase(tmp));
+            dfs(x + 1, s, list);
+            s.setCharAt(x, Character.toUpperCase(tmp));
+            dfs(x + 1, s, list);
         }
+        else
+            dfs(x + 1, s, list);
     }
 
     public static void main(String[] args) {
