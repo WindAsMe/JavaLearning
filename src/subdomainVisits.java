10bfc1e00f121509a9e5e5de79513034083221fb
WindAsMe <542636539@qq.com>
2018-10-06 14:44:38 +0800

    Java For LeetCode

--- src/subdomainVisits.java
+++ src/subdomainVisits.java
@@ -20,17 +20,25 @@ public class subdomainVisits {
         }
         // System.out.println(map.toString());
         for (Map.Entry<String, Integer> entry : map.entrySet()) {
-            System.out.println(entry.getKey());
             String[] domains = entry.getKey().split("\\.");
-            System.out.println(Arrays.toString(domains));
-            for (String domain : domains) {
-                if (helper.get(domain) == null)
-                    helper.put(domain, entry.getValue());
+            for (int i = 0; i < domains.length; i++) {
+                StringBuilder builder = new StringBuilder();
+                for (int i1 = i; i1 < domains.length; i1++) {
+                    if (i1 != domains.length - 1)
+                        builder.append(domains[i1]).append('.');
+                    else
+                        builder.append(domains[i1]);
+                }
+
+                if (helper.get(builder.toString()) == null)
+                    helper.put(builder.toString(), entry.getValue());
                 else
-                    helper.put(domain, helper.get(domain) + entry.getValue());
+                    helper.put(builder.toString(), helper.get(builder.toString()) + entry.getValue());
             }
         }
-        System.out.println(helper.toString());
+        for (Map.Entry<String, Integer> entry : helper.entrySet())
+            list.add(entry.getValue() + " " + entry.getKey());
+        System.out.println(list.toString());
         return list;
     }
 
