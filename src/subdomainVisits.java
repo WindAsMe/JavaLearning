e589f508aa36d7048a0e2ce71abbe6084db2995d
WindAsMe <542636539@qq.com>
2018-10-06 14:47:42 +0800

    Java For LeetCode

--- src/subdomainVisits.java
+++ src/subdomainVisits.java
@@ -11,35 +11,25 @@ public class subdomainVisits {
 
     private static List<String> subdomainVisitsResult(String[] cpdomains) {
         Map<String, Integer> map = new HashMap<>();
-        Map<String, Integer> helper = new HashMap<>();
-        List<String> list = new ArrayList<>();
-        for (String s : cpdomains) {
-            int frequency = Integer.valueOf(s.split(" ")[0]);
-            String domain = s.split(" ")[1];
-            map.put(domain, frequency);
-        }
-        // System.out.println(map.toString());
-        for (Map.Entry<String, Integer> entry : map.entrySet()) {
-            String[] domains = entry.getKey().split("\\.");
-            for (int i = 0; i < domains.length; i++) {
-                StringBuilder builder = new StringBuilder();
-                for (int i1 = i; i1 < domains.length; i1++) {
-                    if (i1 != domains.length - 1)
-                        builder.append(domains[i1]).append('.');
-                    else
-                        builder.append(domains[i1]);
-                }
+        for (String domain : cpdomains) {
+            if (domain.length() == 0)
+                continue;
+            int index = domain.indexOf(' ');
+            int time = Integer.valueOf(domain.substring(0, index));
+            domain = domain.substring(index + 1);
+            map.put(domain, map.getOrDefault(domain, 0) + time);
 
-                if (helper.get(builder.toString()) == null)
-                    helper.put(builder.toString(), entry.getValue());
-                else
-                    helper.put(builder.toString(), helper.get(builder.toString()) + entry.getValue());
+            while (domain.indexOf('.') != -1) {
+                domain = domain.substring(domain.indexOf('.') + 1);
+                map.put(domain, map.getOrDefault(domain, 0) + time);
             }
         }
-        for (Map.Entry<String, Integer> entry : helper.entrySet())
-            list.add(entry.getValue() + " " + entry.getKey());
-        System.out.println(list.toString());
-        return list;
+        List<String> res = new ArrayList<>();
+        for (Map.Entry<String, Integer> entry : map.entrySet()) {
+            String s = entry.getValue() + " " + entry.getKey();
+            res.add(s);
+        }
+        return res;
     }
 
     public static void main(String[] args) {
