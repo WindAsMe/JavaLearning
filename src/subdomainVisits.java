4681f060a5c4156ca42c0c8ea3280a43cad2a746
WindAsMe <542636539@qq.com>
2018-10-06 14:37:46 +0800

    Java For LeetCode

--- src/subdomainVisits.java
+++ src/subdomainVisits.java
@@ -1,6 +1,4 @@
-import java.util.HashMap;
-import java.util.List;
-import java.util.Map;
+import java.util.*;
 
 /**
  * Author     : WindAsMe
@@ -13,13 +11,27 @@ public class subdomainVisits {
 
     private static List<String> subdomainVisitsResult(String[] cpdomains) {
         Map<String, Integer> map = new HashMap<>();
+        Map<String, Integer> helper = new HashMap<>();
+        List<String> list = new ArrayList<>();
         for (String s : cpdomains) {
             int frequency = Integer.valueOf(s.split(" ")[0]);
             String domain = s.split(" ")[1];
             map.put(domain, frequency);
         }
-        System.out.println(map.toString());
-        return null;
+        // System.out.println(map.toString());
+        for (Map.Entry<String, Integer> entry : map.entrySet()) {
+            System.out.println(entry.getKey());
+            String[] domains = entry.getKey().split("\\.");
+            System.out.println(Arrays.toString(domains));
+            for (String domain : domains) {
+                if (helper.get(domain) == null)
+                    helper.put(domain, entry.getValue());
+                else
+                    helper.put(domain, helper.get(domain) + entry.getValue());
+            }
+        }
+        System.out.println(helper.toString());
+        return list;
     }
 
     public static void main(String[] args) {
