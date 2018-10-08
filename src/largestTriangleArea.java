e7cd6e04dab5414b480f2a93f0d469357078f314
WindAsMe <542636539@qq.com>
2018-10-08 20:35:44 +0800

    Java For LeetCode

--- src/largestTriangleArea.java
+++ src/largestTriangleArea.java
@@ -30,6 +30,7 @@ public class largestTriangleArea {
     }
 
     public static void main(String[] args) {
-
+        int[][] p = {{0,0},{0,1},{1,0},{0,2},{2,0}};
+        System.out.println(largestTriangleAreaResult(p));
     }
 }
