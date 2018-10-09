85fa4f4ddc4559c74c7850839e54a403d61d349a
WindAsMe <542636539@qq.com>
2018-10-09 12:58:18 +0800

    Java For LeetCode

--- src/middleNode.java
+++ src/middleNode.java
@@ -23,22 +23,19 @@ public class middleNode {
             len++;
             node = node.next;
         }
-        if (len % 2 == 0)
-            len = len / 2 + 1;
-        else
-            len /= 2;
-        System.out.println(len);
-        return null;
+        len /= 2;
+        node = head;
+        while (len > 0) {
+            len--;
+            node = node.next;
+        }
+        return node;
     }
 
     public static void main(String[] args) {
         ListNode head = new ListNode(1);
         head.next = new ListNode(2);
         head.next.next = new ListNode(3);
-        head.next.next.next  = new ListNode(4);
-        head.next.next.next.next  = new ListNode(5);
-        head.next.next.next.next.next  = new ListNode(6);
-        head.next.next.next.next.next.next  = new ListNode(7);
-        middleNodeResult(head);
+        System.out.println(middleNodeResult(head).val);
     }
 }
